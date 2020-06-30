990e50ad69fc80442f9194090bdfc38af3da625b
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-01 02:47:24 +0300

    lab28

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 
 import Header from './components/header/header';
-import Footer from './components/footer/footer';
 import Form from './components/form/form';
+import Results from './components/results/results';
+import Footer from './components/footer/footer';
 
 
-function App() {
-  return (
-    <>
-    <Header />
-    <Form />
-    <Footer />
-    </>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0, results: [] }
+  }
+
+  handlerForm = (headers, results) => {
+    const result = { headers, response: results }
+    this.setState({ count: results.count, results: result });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <Form prompt='GO!' handler={this.handlerForm} />
+          <Results response={this.state.results} headers={this.state.results.headers} />
+        </main>
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
