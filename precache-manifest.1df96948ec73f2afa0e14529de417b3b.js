d9558bf82add48e76e3e1d1cb7fd3d1eb89b2a84
diana96alazzam <runner@fv-az70.e3cw3d5dqpdexh0nhoz4xotvtb.bx.internal.cloudapp.net>
2020-06-30 12:00:01 +0000

    Updates

---
+++ precache-manifest.1df96948ec73f2afa0e14529de417b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e31e6a16aa89a0a12138f565a263ab3",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "9960d7306897cc8a5912",
+    "url": "/resty/static/css/main.2df537d4.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9960d7306897cc8a5912",
+    "url": "/resty/static/js/main.2c229a02.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
