2162f5bddbab62b3bee045759322c2255243dda2
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-07-06 00:54:15 +0300

    bring the tests from lab 27 to the prev lab

--- src/__tests__/components/form/form.test.js
+++ src/__tests__/components/form/form.test.js
@@ -4,21 +4,32 @@ import renderer from 'react-test-renderer';
 import Form from '../../../components/form/form';
 
 describe('<Form/>', () => {
+
   it('is alive at application start', () => {
     const form = shallow(<Form />);
     expect(form.find('.results .method').exists()).toBeTruthy();
+  });
+   
+  it('is alive at application start', () => {
+    const form = shallow(<Form />);
+    expect(form.find('#GET').text()).toStrictEqual('GET');
+    expect(form.find('#POST').text()).toStrictEqual('POST');
+    expect(form.find('#PUT').text()).toStrictEqual('PUT');
+    expect(form.find('#DELETE').text()).toStrictEqual('DELETE');
   });
 
   it('properly store the users input into state', () => {
     const form = mount(<Form />);
     const getButton = form.find('#POST');
     const textInput = form.find('#textInput');
+    const formTag = form.find('#form');
 
     textInput.simulate('change', { target: { value: 'localhost:3000' } });
     getButton.simulate('click');
+    formTag.simulate('submit');
 
-    expect(form.state('url')).toStrictEqual('localhost:3000');
-    expect(form.state('method')).toStrictEqual('POST');
+    expect(form.state('request').url).toStrictEqual('localhost:3000');
+    expect(form.state('request').method).toStrictEqual('POST');
 
   });
 
@@ -30,7 +41,6 @@ describe('<Form/>', () => {
 
     textInput.simulate('change', { target: { value: 'localhost:3000' } });
     getButton.simulate('click');
-
     formTag.simulate('submit');
 
     expect(form.state('url')).toStrictEqual('');
@@ -50,12 +60,26 @@ describe('<Form/>', () => {
 
     expect(textInput.text()).toStrictEqual('');
     expect(getButton.hasClass('')).toStrictEqual(true);
-
   });
 
-  it('Do the method selectors/checkboxes obey the styling rules', () => {
+  it('render the final result', ()=> {
+    const form = mount(<Form />);
+    const formTag = form.find('#form');
+    const getButton = form.find('#POST');
+    const textInput = form.find('#textInput');  
+
+    textInput.simulate('change', {target:{value:'localhost:3000'}});
+    getButton.simulate('click');
+
+    formTag.simulate('submit');
+
+    expect(form.find('.url').text()).toStrictEqual('localhost:3000');
+    expect(form.find('.method').text()).toStrictEqual('POST');
+  });
+
+  it('properly display the users input in the output area on form submit', () => {
     const rendered = renderer.create(<Form />).toJSON();
     expect(rendered).toMatchSnapshot();
   });
 
-})
+});
